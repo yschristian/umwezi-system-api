7fc92d8d1a2c2cb842117dbc68c2d3d98fba68b6
yschristian <yubahwesc@gmail.com>
2022-10-29 21:52:06 +0200

    user  controller

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -25,27 +25,27 @@ class Usercontroller extends Controller
     public function index(){
 
         $users = User::all();
-        return view ('users.index')->with('users', $users);
+        return $users;
     }
-
+    
     public function show($id){
         $user = User::find($id);
-        return view ('users.show')->with('user',$user);
+        return $user;
     }
 
-    public function edit($id){
-        $user = User::find($id);
-        return view ('users.edit')->with('user',$user);
-    }
+    // public function edit($id){
+    //     $user = User::find($id);
+    //     return view ('users.edit')->with('user',$user);
+    // }
     public function destroy($id){
         $user = User::destroy($id);
-        return view('user')->with('user',$user);
+        return $user;
     }
 
     public function update(Request $request,$id){
         $user = User::find($id);
         $input = $request->all();
-        $user = update($input);
-        return view('user')->with('user',$user);
+        $user-> update($input);
+        return $user;
     }
 }
