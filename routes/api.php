70e6dd378e6249e487357c1f9aa762ef41a8547c
yschristian <yubahwesc@gmail.com>
2022-11-05 12:57:16 +0200

    routes of the system

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ Route::get("/getone/{user}",[\App\Http\Controllers\Usercontroller::class, 'show'
 Route::post("/create",[\App\Http\Controllers\Usercontroller::class, 'store']);
 Route::patch("/update/{user}",[\App\Http\Controllers\Usercontroller::class, 'update']);
 Route::delete("/delete/{user}",[\App\Http\Controllers\Usercontroller::class, 'destroy']);
+Route::post("/login",[\App\Http\Controllers\Usercontroller::class, 'login']);
 
 });
 //requests
@@ -31,4 +32,8 @@ Route::prefix('request')->group(function(){
 // products
 Route::prefix('product')->group(function(){
     Route::post("/create",[\App\Http\Controllers\ProductController::class, 'store']);
+    Route::get("/getAll",[\App\Http\Controllers\ProductController::class, 'index']);
+    Route::get("/getOne/{product}",[\App\Http\Controllers\ProductController::class, 'show']);
+    Route::delete("/delete/{product}",[\App\Http\Controllers\ProductController::class, 'destroy']);
+    Route::patch("/update/{product}",[\App\Http\Controllers\ProductController::class, 'update']);
 });
