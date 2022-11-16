28f6c73df8c94af1512694c2086819d211c15e55
yschristian <yubahwesc@gmail.com>
2022-11-16 11:18:57 +0200

    forgot password

--- resources/views/components/forgotPassword.blade.php
+++ resources/views/components/forgotPassword.blade.php
@@ -1,29 +1,14 @@
 <!DOCTYPE html>
-
-<!--
- // WEBSITE: https://themefisher.com
- // TWITTER: https://twitter.com/themefisher
- // FACEBOOK: https://www.facebook.com/themefisher
- // GITHUB: https://github.com/themefisher/
--->
-
 <html lang="en">
 <head>
-
-  <!-- Basic Page Needs
-  ================================================== -->
   <meta charset="utf-8">
   <title>UMWEZI FARMING</title>
-
-  <!-- Mobile Specific Metas
-  ================================================== -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="description" content="Construction Html5 Template">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
   <meta name="author" content="Themefisher">
   <meta name="generator" content="Themefisher Constra HTML Template v1.0">
   
-  <!-- theme meta -->
   <meta name="theme-name" content="aviato" />
   
   <!-- Favicon -->
