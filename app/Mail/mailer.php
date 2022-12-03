620f2c1c67e21b7470d46000aa52e98859263f3e
yschristian <yubahwesc@gmail.com>
2022-12-03 15:06:15 +0200

    mail

--- app/Mail/mailer.php
+++ app/Mail/mailer.php
@@ -12,54 +12,25 @@ use Illuminate\Queue\SerializesModels;
 class mailer extends Mailable
 {
     use Queueable, SerializesModels;
+    private $data = [];
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
+   
+    public function __construct($data)
     {
-        //
+        $this->data=$data;
     }
 
-    /**
-     * Get the message envelope.
-     *
-     * @return \Illuminate\Mail\Mailables\Envelope
-     */
-    public function envelope()
-    {
-        return new Envelope(
-            subject: 'Mailer',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     *
-     * @return \Illuminate\Mail\Mailables\Content
-     */
-    public function content()
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array
-     */
-    public function attachments()
-    {
-        return [];
-    }
+//     public function build()
+//     {
+//         return $this->from('finefood@gmail.com','Fine Food Restaurant')
+//         ->subject($this->data['subject'])
+//         ->view('emails.index')->with('data',$this->data)
+// ;
+//     }
     public function build()
     {
         return $this -> from('umuhozaaimee12@gmail.com','UMWEZIFS')
         ->subject($this->data['subject'])
-        ->view('Emails')->with('data',$this->data);
+        ->view('emails.index')->with('data',$this->data);
     }
 }
